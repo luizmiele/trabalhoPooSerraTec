3b6e0562f67e5e0fda38201d9a1a7f2575ec5e0d
luizmiele <luiz.miele@hotmail.com>
2024-04-17 00:03:06 -0300

    FOI UM COMMIT APENAS PARA LIMPAR AS ANOTACOES DA MAIN (JURO POR DEUS)

--- TrabalhoFinalPoo/src/poo/trabalho/serratec/main/Main.java
+++ TrabalhoFinalPoo/src/poo/trabalho/serratec/main/Main.java
@@ -5,31 +5,6 @@ import poo.trabalho.serratec.menu.MenuLogin;
 public class Main {
 
 	public static void main(String[] args) {
-		//System.out.println(PlanoDAO.getListaPlanos());
-		
-		//System.out.println(PlanoDAO.getNomePlanoByID(8)); 
 		MenuLogin.login();
-		
-		// PEDIR TODAS AS INFORMACOES INDIVIDUALEMTENTE. anuloLOCAL(nome, cpf,...) 
-		// Aluno aluno1 = new Aluno();
-		//aluno.cadastra(alunoPURAO);
-		
-		
-		//Aluno aluno1 = new Aluno();
-		
-		// Aluno alunoPURAO = new Aluno("Lucas Amorin2", "123", LocalDate.of(2007, 2, 11), "11111-1111", "lucas@amorin.com.br", "456", "Aluno", "PINK", LocalDate.of(2024, 4,12));
-		
-		
-		
-		//AlunoDAO aluno = new AlunoDAO();
-		
-		//aluno.cadastra(alunoPURAO);
-	
-		
-		//Funcionario funcionarioOBJ = new Funcionario("Boeck", "12332112332", LocalDate.of(2000, 4, 17), "11111-1111", "Boeck@Academia.com", "1234", "Funcionario", "Gerente mar brabo");
-		
-		//FuncionarioDAO funcionario = new FuncionarioDAO();
-		
-		//funcionario.cadastra(funcionarioOBJ);
 	}
 }
